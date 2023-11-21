2a9a68d94b965d2b5f36e574b63c8e75c86e5fa0
dreamingwhale <dreamingskywhale@gmail.com>
2023-11-21 15:10:32 +0900

    20231121 콜백 함수 등

--- main.cpp
+++ main.cpp
@@ -1,70 +1,117 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
-int Add(int A, int B)
-{
-	cout << A + B << endl;
 
-	return A + B;
+
+class StaticTest
+{
+private:
+	StaticTest()
+	{
+		Count++;
+		PrintCount();
+	}
+public:
+	~StaticTest()
+	{
+		Count--;
+		PrintCount();
+	}
+	static StaticTest* GetInstance()
+	{
+		if (Instance == nullptr)
+		{
+			Instance = new StaticTest();
+		}
+
+		return Instance;
+	}
+private:
+	static void PrintCount()
+	{
+		cout << Count << endl;
+	}
+
+	static int Count;
+
+	static StaticTest* Instance;
+};
+
+int StaticTest::Count = 0;
+StaticTest* StaticTest::Instance = nullptr;
+
+
+class ConsturctSample
+{
+public:
+	ConsturctSample(int NewNumber, char* NewName)
+	{
+		Number = NewNumber;
+		Name = string(NewName);
+	}
+	ConsturctSample(const ConsturctSample& InOrigin)
+	{
+		cout << "Copy" << endl;
+		Number = InOrigin.Number;
+		//Name = string(Origin.Name);
+		Name = InOrigin.Name;
+	}
+	virtual ~ConsturctSample()
+	{
+
+	}
+
+	int Number;
+	string Name;
+};
+
+void Swap(int& A, int& B)
+{
+	int Temp = A;
+	A = B;
+	B = Temp;
 }
 
-int Sub(int A, int B)
+void Add(int A = 20, int B = 10, int C = 0)
 {
-	cout << A - B << endl;
 
-	return A - B;
-}
-
-//C#, java
-//delegate
-//delegate
-int (*FunctionPointer)(int A, int B);
-
-//callback
-void LoadAsync(int A, int B, int (*WhenComplete)())
-{
-	//Network
-	//Loading Loading;
-	WhenComplete();
-}
-
-int Desending(const void* PA, const void* PB)
-{
-	int A = *(int*)PA;
-	int B = *(int*)PB;
-
-	return B - A;
-}
-
-
-int Asending(const void* PA, const void* PB)
-{
-	int A = *(int*)PA;
-	int B = *(int*)PB;
-
-	return A - B;
 }
 
 int main()
 {
-	int Number[10] = { 1, 10, 2, 5, 4, 3, 8, 9, 7, 6 };
-	qsort(Number, 10, 4, Asending);
+	int A = 10;
+	int B = 20;
 
-	//for (auto D : Number)
-	//{
-	//	cout << D << endl;
-	//}
+	Add(A, B, 10);
 
 
-	FunctionPointer = [](int A, int b) {
-		return 1;
-		};
-	//FunctionPointer = Sub;
-	cout << FunctionPointer(10, 20) << endl;
+	cout << A << endl;
+	cout << B << endl;
+
+	//char Message[] = "HelloWorld";
+	//ConsturctSample C1(300, Message);
+	//ConsturctSample C2 = C1;
+
+	//string A = "World";
+	//string B = string(A);
+	//
+	//cout << A << endl;
+	//A[0] = 'C';
+	//cout << B << endl;
+
+	//cout << C1.Name << endl;
+	//Message[5] = 'A';
+	//cout << C2.Name << endl;
+
+	//cout << C1.Number << endl;
+	//C1.Number = 100;
+	//cout << C2.Number << endl;
+
+
+
 
 	return 0;
 }
